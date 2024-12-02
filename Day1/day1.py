98b10d062e7e736470e867e2e411892f76099f4b
Antti <antti.niiranen963@hotmail.fi>
2024-12-02 20:24:11 +0200

    Part 1

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,30 +1,21 @@
 import numpy as np
 import pandas as pd
 
-#siirretään data csv muotoon
 inputdata = pd.read_csv("input.txt", sep=" ", header=None)
-inputdata.to_csv("input.csv")
 
-#alustetaan listat muistiin
 list1 = np.zeros(len(inputdata))
 list2 = np.zeros(len(inputdata))
 
-# siirretään data omiin listoihin
 for i in range(0,len(inputdata),1):
     list1[i] = inputdata.iloc[i,0]
     list2[i] = inputdata.iloc[i,3]
 
-
-# järjetetään data suuruusjärjestykseen
 sorted1 = np.sort(list1, axis=None)
 sorted2 = np.sort(list2, axis=None)
-result = np.zeros(len(sorted1))
+result = np.zeros(len(inputdata))
 
-# lasketaan arvojen etäisyys ja summa
 for i in range(0,len(list1),1):
-    result[i] = sorted1[i] - sorted2[i]
-    result[i] = np.abs(result[i])
-
+    result[i] = np.abs(sorted1[i] - sorted2[i])
 sum = np.cumulative_sum(result)
 print(sum[len(sum)-1])
 
